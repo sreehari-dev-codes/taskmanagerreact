36271cb88831fd8235ddf3eee292d233f87e38ec
sreehari <ramachanmoshedayan@gmail.com>
2025-03-13 19:11:48 +0530

    updated code

--- frontend/src/Components/Error/Error.jsx
+++ frontend/src/Components/Error/Error.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 
 function Error() {
   return (
-    <div>
-        <h1>not accesing</h1>
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Oops! Page Not Found</h1>
+      <img
+        src="https://img.freepik.com/free-vector/400-error-bad-request-concept-illustration_114360-1921.jpg?uid=R156682381&ga=GA1.1.539061890.1721370777&semt=ais_hybrid"
+        alt="404 Error"
+        style={{ width: "50%", maxWidth: "500px", marginTop: "20px" }}
+      />
     </div>
-  )
+  );
 }
 
-export default Error
+export default Error;
